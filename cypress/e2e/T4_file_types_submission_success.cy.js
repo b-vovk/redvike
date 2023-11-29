c408f94b62d531c9187e3003290f2a41f545a997
Bogdan Vovk <me.vovk@gmail.com>
2023-11-29 02:05:45 -0800

    check data types on the upload file

---
+++ cypress/e2e/T4_file_types_submission_success.cy.js
@@ -0,0 +1,46 @@
+import ApplicationForm from '../pages/application_form'
+import SuccessPage from '../pages/success'
+
+const application_form = new ApplicationForm()
+const success_page = new SuccessPage()
+
+const first_name = 'Auto'
+const last_name = 'Tester'
+const email = 'auto_tester@redvike.com'
+const password = 'password'
+
+const files = ['/avatar.gif', '/avatar.jpeg', '/avatar.bmp', '/avatar.jpg']
+
+files.forEach((file) => {
+  describe('Application form', () => {
+    beforeEach(() => {
+      cy.visit('/')
+    })
+    it(`Submit application form successfully with ${file} type`, () => {
+      cy.step('Enter user data')
+      application_form.enter_first_name(first_name)
+      application_form.enter_last_name(last_name)
+      application_form.enter_email(email)
+      application_form.enter_password(password)
+      application_form.confirm_password(password)
+
+      cy.step('Upload the file')
+      application_form.upload_file(file)
+
+      cy.step('Slide thumb')
+      application_form.slide_thumb()
+
+      cy.step('Click the Submit button')
+      application_form.click_submit_button()
+
+      cy.step('Verify the form is submited sucesfully')
+      success_page.verify_confirmation_text_visible()
+      cy.contains(first_name)
+      cy.contains(last_name)
+      cy.contains(email)
+
+      cy.step('Verify the avatar image is visible')
+      success_page.verify_avatar_img_visible()
+    })
+  })
+})
